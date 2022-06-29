5c9cb9333c2bafa9806eddb1aae069529eff19bb
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-06-29 20:08:08 +0200

    build: setup eslint and prettier
    + configure src dir

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 import { defineNuxtConfig } from 'nuxt'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  ssr: false
+  dir: {
+    public: '../public',
+  },
+  srcDir: 'src',
+  ssr: false,
+  typescript: {
+    strict: true,
+  },
+  vite: {
+    plugins: [eslintPlugin()],
+  },
 })
---
+++ src/app.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <Head>
+      <Title>Collaboration Tools</Title>
+      <Link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/images/apple-touch-icon.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/images/favicon-32x32.png"
+      />
+      <Link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/images/favicon-16x16.png"
+      />
+      <Link rel="manifest" href="/site.webmanifest" />
+      <Link
+        rel="mask-icon"
+        href="/images/safari-pinned-tab.svg"
+        color="#5b21b6"
+      />
+      <Link rel="shortcut icon" href="/images/favicon.ico" />
+      <Meta name="msapplication-TileColor" content="#5b21b6" />
+      <Meta name="msapplication-config" content="/browserconfig.xml" />
+      <Meta name="theme-color" content="#5b21b6" />
+    </Head>
+    <h1>Collaboration Tools</h1>
+  </div>
+</template>

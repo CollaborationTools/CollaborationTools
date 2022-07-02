ba95bdfa101e6f6556a900ecf7f651e60809a383
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-02 14:42:38 +0200

    refactor: move head section to separate component

--- src/app.vue
+++ src/app.vue
@@ -1,35 +1,6 @@
 <template>
-  <div class="grid h-screen place-items-center text-">
-    <Head>
-      <Title>Collaboration Tools</Title>
-      <Link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/images/apple-touch-icon.png"
-      />
-      <Link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/images/favicon-32x32.png"
-      />
-      <Link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/images/favicon-16x16.png"
-      />
-      <Link rel="manifest" href="/site.webmanifest" />
-      <Link
-        rel="mask-icon"
-        href="/images/safari-pinned-tab.svg"
-        color="#5b21b6"
-      />
-      <Link rel="shortcut icon" href="/images/favicon.ico" />
-      <Meta name="msapplication-TileColor" content="#5b21b6" />
-      <Meta name="msapplication-config" content="/browserconfig.xml" />
-      <Meta name="theme-color" content="#5b21b6" />
-    </Head>
+  <div>
+    <TheHead />
     <div class="prose">
       <AtomLogo class="mb-8" />
       <h1 class="text-violet-800 dark:text-white text-center">

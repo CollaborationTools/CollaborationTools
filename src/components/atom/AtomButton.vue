77adbfb2686d732138ea2c48a076778cc58389c9
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-02 18:30:44 +0200

    feat: add AtomButton

---
+++ src/components/atom/AtomButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <component
+    :is="to ? Link : 'button'"
+    :to="to"
+    :role="to ? 'button' : undefined"
+    :class="[
+      'btn',
+      { 'btn-accent': accent && !primary && !secondary },
+      { 'btn-secondary': secondary && !primary },
+      { 'btn-primary': !outline && primary },
+      outline && primary
+        ? 'btn bg-transparent ' +
+          'text-primary dark:text-violet-400 ' +
+          'border-primary dark:border-violet-400 ' +
+          'hover:text-white dark:hover:text-white ' +
+          'hover:bg-violet-900 dark:hover:bg-violet-900 ' +
+          'hover:border-transparent dark:hover:border-transparent'
+        : '',
+      { 'btn-disabled text-gray-600 dark:text-gray-300': disabled },
+    ]"
+    ><slot
+  /></component>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  to?: string
+  primary?: boolean
+  secondary?: boolean
+  accent?: boolean
+  outline?: boolean
+  disabled?: boolean
+}
+
+defineProps<Props>()
+
+const Link = defineNuxtLink({
+  componentName: 'AtomButtonLink',
+  externalRelAttribute: '',
+  activeClass: '',
+  exactActiveClass: '',
+})
+</script>

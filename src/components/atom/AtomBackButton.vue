bab65ab5f0e85c9936463c6bc52cc1a204847441
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-03 20:06:47 +0200

    feat: add AtomBackButton

---
+++ src/components/atom/AtomBackButton.vue
@@ -0,0 +1,11 @@
+<template>
+  <AtomButton v-if="hasHistory" primary outline @click="goBack"
+    >Go back</AtomButton
+  >
+</template>
+
+<script setup lang="ts">
+const router = useRouter()
+const hasHistory = !!router.options.history.state.back
+const goBack = (): void => router.back()
+</script>

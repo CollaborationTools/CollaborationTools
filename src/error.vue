ee9f1bc5351228b2e8e3a6d5194b71e794c5dd4d
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-03 20:07:39 +0200

    ui: add error page
    + add AtomDot

---
+++ src/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="grid h-screen place-items-center p-16">
+    <main class="flex flex-col max-w-md prose text-center">
+      <h2 class="mb-3 font-extrabold text-9xl text-gray-300 dark:text-gray-600">
+        <span class="sr-only">Error</span>
+        404
+      </h2>
+      <p class="mb-0 text-2xl font-semibold md:text-3xl">
+        Sorry, we couldn't find this page<AtomDot />
+      </p>
+      <p>But don't worry, you can find plenty of other things around.</p>
+      <div>
+        <AtomBackButton class="m-4" />
+        <AtomButton to="/" primary outline class="m-4"
+          >Go to homepage</AtomButton
+        >
+      </div>
+    </main>
+  </div>
+</template>

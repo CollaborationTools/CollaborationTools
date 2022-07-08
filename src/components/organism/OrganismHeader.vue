2d34c6fe81fc553fe68eaabe64e4867c6a18d84d
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-08 20:54:26 +0200

    feat: add OrganismHeader

---
+++ src/components/organism/OrganismHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header class="navbar bg-white dark:bg-base-300 shadow">
+    <h1 class="flex-1 px-4 text-xl">{{ orgName }}<AtomDot /></h1>
+    <div class="flex-none">
+      <MoleculeColorMode />
+    </div>
+  </header>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  orgName: string
+}
+
+const props = defineProps<Props>()
+</script>

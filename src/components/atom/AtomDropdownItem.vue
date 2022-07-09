07de37019e06c02b15fbd50101b3d7633753c6f6
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-09 17:09:02 +0200

    ui: add MoleculeDropdownNav
    + AtomDropdownItem

---
+++ src/components/atom/AtomDropdownItem.vue
@@ -0,0 +1,19 @@
+<template>
+  <li>
+    <NuxtLink :to="url" :class="{ 'justify-between': badge }">
+      <slot />
+      <span v-if="badge" class="badge badge-secondary font-semibold">{{
+        badge
+      }}</span>
+    </NuxtLink>
+  </li>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  url?: string
+  badge?: string | number
+}
+
+const props = defineProps<Props>()
+</script>
---
+++ src/components/molecule/MoleculeDropdownNav.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="dropdown">
+    <label tabindex="0" class="btn btn-ghost">
+      <span class="!normal-case text-xl">{{ orgName }}<AtomDot /></span>
+      <AtomIcon name="chevron-down" class="ml-2" />
+    </label>
+    <ul
+      class="menu dropdown-content mt-6 p-2 shadow bg-white dark:bg-base-300 rounded-box min-w-max max-w-sm"
+    >
+      <AtomDropdownItem
+        v-for="org in sampleOrgData"
+        :key="org.name"
+        :url="org.url"
+        :badge="org.notifications"
+        >{{ org.name }}</AtomDropdownItem
+      >
+      <li class="divider"></li>
+      <li>
+        <a href="/org/new">
+          <AtomIcon name="plus" class="mr-0" />
+          Create new org
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  orgName: string
+}
+
+const props = defineProps<Props>()
+
+const sampleOrgData = [
+  { url: '/', name: 'Remote organisation', notifications: 1 },
+  { url: '/', name: 'Sample group', notifications: 0 },
+  { url: '/', name: 'Local cooperative', notifications: 3 },
+  { url: '/', name: 'Another organisation', notifications: 101 },
+]
+</script>
+
+<style scoped lang="postcss">
+.divider:after {
+  @apply hidden;
+}
+</style>

0f4396eb07d2b1f95cc463db40653e0e574221a3
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-11 15:31:33 +0200

    e2e: add tests for browsing existing orgs
    + prevent failing of test cases due to automated localStorage clearing between test cases

--- test/cypress/e2e/basic/member.cy.ts
+++ test/cypress/e2e/basic/member.cy.ts
@@ -3,8 +3,13 @@ import member1 from 'cypress/fixtures/userStorage/member-1'
 
 const memberOrganisations = member1.get('organisations')
 const memberRecentOrganisations = member1.get('recentOrganisations')
+
 const currentOrgId = memberRecentOrganisations[0]
-const orgName = 'Org 4'
+const currentOrgName = memberOrganisations.get(currentOrgId).name
+const nextOrgId = memberRecentOrganisations[1]
+const nextOrgName = memberOrganisations.get(nextOrgId).name
+
+const newOrgName = 'Org 4'
 
 describe('a member can', () => {
   beforeEach(() => {
@@ -12,15 +17,18 @@ describe('a member can', () => {
     cy.visit('/')
   })
 
-  it('create another org', () => {
+  it('browse existing orgs', () => {
     cy.url().should('contain', '/org/' + currentOrgId)
-    cy.getId('org-name')
+    cy.getId('org-name').should('contain.text', currentOrgName).click()
+    cy.getUUID(nextOrgId).should('contain.text', nextOrgName).click()
+    cy.url().should('contain', '/org/' + nextOrgId)
+    cy.getId('org-name').should('contain.text', nextOrgName)
+  })
 
-      .should('contain.text', memberOrganisations.get(currentOrgId).name)
-      .click()
-
+  it('create another org', () => {
+    cy.getId('org-name').should('be.visible').click()
     cy.getId('create-new-org').click()
     cy.url().should('contain', '/org/new')
-    cy.handleOrgCreation(orgName)
+    cy.handleOrgCreation(newOrgName)
   })
 })
--- test/cypress/support/e2e.ts
+++ test/cypress/support/e2e.ts
@@ -1,3 +1,9 @@
 import './formCommands'
 import './storageCommands'
 import './uiCommands'
+
+// as long as reactive localStorage is used in the app exceptions should not fail tests
+// issue occurs because Cypress automatically clears localStorage in between test cases
+Cypress.on('uncaught:exception', () => {
+  return false
+})

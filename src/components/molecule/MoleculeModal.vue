5ab3ed48c41b54c97253b54a9a51babb7edb121b
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-22 15:15:37 +0200

    ui: add profile creation

---
+++ src/components/molecule/MoleculeModal.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="contents">
+    <AtomButton primary @click="isModalOpen = true">{{ label }}</AtomButton>
+
+    <Teleport to="body">
+      <!-- eslint-disable-next-line vuejs-accessibility/click-events-have-key-events -->
+      <div
+        v-if="isModalOpen"
+        :class="{
+          'modal modal-bottom sm:modal-middle': true,
+          'modal-open': isModalOpen,
+        }"
+        @click="cancelAction"
+      >
+        <div class="modal-box grid gap-4" @click.stop>
+          <slot />
+          <div class="contents">
+            <AtomButton primary outline @click="cancelAction">{{
+              cancelLabel
+            }}</AtomButton>
+            <AtomButton primary @click="confirmAction">{{
+              confirmLabel
+            }}</AtomButton>
+          </div>
+        </div>
+      </div>
+    </Teleport>
+  </div>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  label: string
+  modelValue: boolean
+  cancelLabel?: string
+  confirmLabel?: string
+}
+
+type Emits = {
+  (eventName: 'cancel'): void
+  (eventName: 'confirm'): void
+  (eventName: 'update:modelValue', modelValue: boolean): void
+}
+
+const {
+  label,
+  modelValue,
+  cancelLabel = 'Cancel',
+  confirmLabel = 'Confirm',
+} = defineProps<Props>()
+const emit = defineEmits<Emits>()
+
+const isModalOpen = computed({
+  get() {
+    return modelValue
+  },
+  set(newValue) {
+    emit('update:modelValue', newValue)
+  },
+})
+const cancelAction = (): void => {
+  emit('cancel')
+  isModalOpen.value = false
+}
+const confirmAction = (): void => {
+  emit('confirm')
+}
+</script>
---
+++ src/components/organism/OrganismCreateUser.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="contents">
+    <MoleculeModal
+      v-model="isModalOpen"
+      label="Create profile"
+      @cancel="resetFields"
+      @confirm="createProfile"
+    >
+      <h3 class="font-bold text-lg text-center">Create your profile</h3>
+      <AtomInput
+        v-model="state.profileName"
+        label="Full name"
+        data-id="user-name-field"
+        :errors="v$.profileName.$errors"
+        @blur="v$.profileName.$touch"
+      />
+      <AtomInput
+        v-model="state.preferredName"
+        label="Display name in current organisation (optional, if different than full name)"
+        data-id="display-name-field"
+      />
+    </MoleculeModal>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useVuelidate } from '@vuelidate/core'
+import { required } from '@vuelidate/validators'
+
+import { createOrganisationMember } from '@/core/user'
+import useOrganisations from '@/stores/useOrganisations'
+import useUsers from '@/stores/useUsers'
+
+const isModalOpen = ref(false)
+
+const userStore = useUsers()
+
+const state = reactive({
+  preferredName: '',
+  profileName: '',
+})
+const rules = {
+  profileName: { required },
+}
+
+const v$ = useVuelidate(rules, state)
+
+const resetFields = (): void => {
+  state.preferredName = ''
+  state.profileName = ''
+  v$.value.$reset()
+}
+
+const createProfile = async (): Promise<void> => {
+  const result = await v$.value.$validate()
+  const currentOrganisation = useOrganisations().getCurrentOrganisation()
+  if (!result || currentOrganisation === null) {
+    return
+  }
+
+  const user = userStore.setMe(state.profileName)
+
+  const preferredName =
+    state.preferredName.length > 0 ? state.preferredName : undefined
+  const organisationMember = createOrganisationMember(
+    user,
+    'admin',
+    preferredName,
+  )
+  userStore.setOrganisationMember(currentOrganisation.id, organisationMember)
+
+  isModalOpen.value = false
+}
+</script>

d038a14fc06124cae773ab98357fdddd4b086647
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-23 18:16:50 +0200

    ui: improve styles for short text in InfoBox

--- src/components/atom/AtomInfoBox.vue
+++ src/components/atom/AtomInfoBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="alert alert-info flex-row">
     <AtomIcon name="info" class="w-6 h-6 min-w-max" />
-    <div class="block !mt-0"><slot /></div>
+    <div class="block !mt-0 mx-auto"><slot /></div>
   </div>
 </template>

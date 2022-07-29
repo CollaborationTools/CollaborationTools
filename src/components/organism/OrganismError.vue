6d667bfd7cbf74c3eb3f30938ec418092d41b5ca
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-30 00:24:54 +0200

    refactor: move error page content to reusable component

---
+++ src/components/organism/OrganismError.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="flex flex-col max-w-md prose prose-sm md:prose-base text-center">
+    <h2 class="!mb-3 font-extrabold !text-9xl text-gray-300 dark:text-gray-600">
+      <span class="sr-only">Error</span>
+      {{ error }}
+    </h2>
+    <p class="!mb-0 text-2xl font-semibold md:text-3xl">
+      {{ errorMessage }}<AtomDot />
+    </p>
+    <p>{{ recommendedAction }}</p>
+    <div>
+      <AtomBackButton class="mr-4" />
+      <AtomButton to="/" primary outline class="m-4">Go to homepage</AtomButton>
+    </div>
+  </main>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  error: string
+  errorMessage: string
+  recommendedAction: string
+}
+
+defineProps<Props>()
+</script>
--- src/error.vue
+++ src/error.vue
@@ -1,25 +1,10 @@
 <template>
   <NuxtLayout name="center">
-    <main
-      class="flex flex-col max-w-md prose prose-sm md:prose-base text-center"
-    >
-      <h2
-        class="!mb-3 font-extrabold !text-9xl text-gray-300 dark:text-gray-600"
-      >
-        <span class="sr-only">Error</span>
-        404
-      </h2>
-      <p class="!mb-0 text-2xl font-semibold md:text-3xl">
-        Sorry, we couldn't find this page<AtomDot />
-      </p>
-      <p>But don't worry, you can find plenty of other things around.</p>
-      <div>
-        <AtomBackButton class="mr-4" />
-        <AtomButton to="/" primary outline class="m-4"
-          >Go to homepage</AtomButton
-        >
-      </div>
-    </main>
+    <OrganismError
+      error="404"
+      error-message="Sorry, we couldn't find this page"
+      recommended-action="But don't worry, you can find plenty of other things around."
+    />
   </NuxtLayout>
 </template>
 

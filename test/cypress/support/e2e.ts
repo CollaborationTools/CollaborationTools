68cf0b938cc1eb0927f0fbff4c1d29b5568c0f28
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-07-29 19:15:21 +0200

    e2e: setup cypress-real-events

--- test/cypress/support/e2e.ts
+++ test/cypress/support/e2e.ts
@@ -1,6 +1,7 @@
 import './formCommands'
 import './storageCommands'
 import './uiCommands'
+import 'cypress-real-events/support'
 
 // as long as reactive localStorage is used in the app exceptions should not fail tests
 // issue occurs because Cypress automatically clears localStorage in between test cases

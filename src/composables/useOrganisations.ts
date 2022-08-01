a86ced255ada238c3a7c4130fbcf8ef93a7dfb44
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-08-01 13:50:05 +0200

    feat: create organisation event

---
+++ src/composables/useOrganisations.ts
@@ -0,0 +1,27 @@
+import { createEvent } from '@/core/connector'
+import { Organisation } from '@/core/organisation'
+import { OrganisationMemberId } from '@/core/user'
+
+type UseOrganisations = {
+  createOrganisationEvent: (
+    senderId: OrganisationMemberId,
+    organisation: Organisation,
+  ) => string
+}
+
+export default function useOrganisations(): UseOrganisations {
+  const createOrganisationEvent = (
+    senderId: OrganisationMemberId,
+    organisation: Organisation,
+  ): string => {
+    const data = JSON.stringify(organisation)
+    const event = createEvent({
+      data,
+      senderId,
+      type: 'organisation',
+    })
+    return JSON.stringify(event)
+  }
+
+  return { createOrganisationEvent }
+}

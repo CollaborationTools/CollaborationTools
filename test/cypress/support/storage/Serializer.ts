fd834c938ff8e5788fa286ad22839d5c87e5e752
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-11-01 15:47:48 +0100

    test: improve code comment to not trigger SonarLint warning

--- test/cypress/support/storage/Serializer.ts
+++ test/cypress/support/storage/Serializer.ts
@@ -9,28 +9,30 @@ import { ORGANISATION_MEMBERS_KEY, USER_PROFILE_KEY } from 'stores/useUserStore'
 
 type Item = string | number | boolean | object | null
 
-// StorageSerializerTypes
-//   return rawInit == null
-//     ? 'any'
-//     : rawInit instanceof Set
-//     ? 'set'
-//     : rawInit instanceof Map
-//     ? 'map'
-//     : rawInit instanceof Date
-//     ? 'date'
-//     : typeof rawInit === 'boolean'
-//     ? 'boolean'
-//     : typeof rawInit === 'string'
-//     ? 'string'
-//     : typeof rawInit === 'object'
-//     ? 'object'
-//     : Array.isArray(rawInit)
-//     ? 'object'
-//     : !Number.isNaN(rawInit)
-//     ? 'number'
-//     : 'any'
-// }
-
+/**
+ * Defines how data is converted between stores and localStorage
+ *
+ * Storage serializers are based on VueUse implementation and its types:
+ *   return rawInit == null
+ *     ? 'any'
+ *     : rawInit instanceof Set
+ *     ? 'set'
+ *     : rawInit instanceof Map
+ *     ? 'map'
+ *     : rawInit instanceof Date
+ *     ? 'date'
+ *     : typeof rawInit === 'boolean'
+ *     ? 'boolean'
+ *     : typeof rawInit === 'string'
+ *     ? 'string'
+ *     : typeof rawInit === 'object'
+ *     ? 'object'
+ *     : Array.isArray(rawInit)
+ *     ? 'object'
+ *     : !Number.isNaN(rawInit)
+ *     ? 'number'
+ *     : 'any'
+ */
 const storageSerializer = {
   [ORGANISATIONS_KEY]: StorageSerializers.map,
   [RECENT_ORGANISATIONS_KEY]: StorageSerializers.object,

50b922cbc2baffa3a802fc0ca493d557ad94367f
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-11-06 01:51:34 +0100

    feat: set status closed for connections with unavailable peer

--- platform/services/p2p/PeerEvent/ServerConnectionEventListener.ts
+++ platform/services/p2p/PeerEvent/ServerConnectionEventListener.ts
@@ -1,3 +1,5 @@
+import { DataConnection } from 'peerjs'
+
 import { DataConnectionEventType } from './DataConnectionEvent'
 import { attachDataConnectionEventListeners } from './DataConnectionEventListener'
 import { MediaConnectionEventType } from './MediaConnectionEvent'
@@ -7,6 +9,7 @@ import { ServerEventType } from './ServerConnectionEvent'
 import {
   createDataConnection,
   createMediaConnection,
+  getConnectionId,
   PeerConnectionStatus,
 } from '../PeerConnection'
 import { PeerConnector, PeerError } from '../PeerConnector'
@@ -42,13 +45,40 @@ export const attachServerEventListeners = (
   })
 
   peerConnector.peerJS.on('error', (error: PeerError) => {
-    peerConnector.eventHandler({
-      type: ServerEventType.ServerError,
-      error: {
-        type: String(error?.type ?? 'unknown-error'),
-        message: error.message,
-      },
-    })
+    if (error.type === 'peer-unavailable') {
+      const remoteDeviceId = error.message.slice(-36)
+      const connectionId = getConnectionId(
+        peerConnector.currentDeviceId,
+        remoteDeviceId,
+      )
+      const connection = peerConnector.connections.get(connectionId)
+
+      if (!connection) {
+        return
+      }
+
+      const newConnection = createDataConnection(
+        // eslint-disable-next-line total-functions/no-unsafe-type-assertion
+        connection.peerConnection as DataConnection,
+        PeerConnectionStatus.Closed,
+      )
+
+      peerConnector.eventHandler({
+        type: DataConnectionEventType.PeerClosed,
+        data: {
+          connection: newConnection,
+        },
+      })
+      peerConnector.connections.set(connectionId, newConnection)
+    } else {
+      peerConnector.eventHandler({
+        type: ServerEventType.ServerError,
+        error: {
+          type: String(error?.type ?? 'unknown-error'),
+          message: error.message,
+        },
+      })
+    }
   })
 
   peerConnector.peerJS.on('connection', (dataConnection) => {

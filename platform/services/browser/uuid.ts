f60243daefd90d82aca9f41279a32dd6d997d33a
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-11-12 18:14:24 +0100

    fix: browser on mobile wasn't generating uuids

--- platform/services/browser/uuid.ts
+++ platform/services/browser/uuid.ts
@@ -1,1 +1,6 @@
-export const createUUID = (): string => window.crypto.randomUUID()
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore - no types for latest version of uuid lib
+import { v4 as uuidV4 } from 'uuid'
+
+export const createUUID = (): string =>
+  window.crypto.randomUUID ? window.crypto.randomUUID() : uuidV4()

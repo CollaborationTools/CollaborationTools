3029f77914d9343cd57d92f498b7818a13f1ad1a
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-11-13 16:50:20 +0100

    feat: add optional space context for direct chat model

--- platform/core/tool/chat/Chat/DirectChat.ts
+++ platform/core/tool/chat/Chat/DirectChat.ts
@@ -1,15 +1,20 @@
-import { ChatId, ParticipantId } from './Chat'
+import { ChatId, ParticipantId, SpaceId } from './Chat'
 
 export type DirectChat = Readonly<{
   id: ChatId
   participant1: ParticipantId
   participant2: ParticipantId
+  spaceId?: SpaceId
 }>
 
-type DirectChatProps = Pick<DirectChat, 'id' | 'participant1' | 'participant2'>
+type DirectChatProps = Pick<
+  DirectChat,
+  'id' | 'participant1' | 'participant2' | 'spaceId'
+>
 
 export const createDirectChat = ({
   id,
   participant1,
   participant2,
-}: DirectChatProps): DirectChat => ({ id, participant1, participant2 })
+  spaceId,
+}: DirectChatProps): DirectChat => ({ id, participant1, participant2, spaceId })

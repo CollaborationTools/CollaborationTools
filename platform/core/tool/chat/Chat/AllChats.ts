4269fa572fec0d9bb03a2b7fbf273256083711b1
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-11-14 00:55:52 +0100

    feat: make AllChats more generic to handle only direct chats
    + add map to array conversion of AllChats

--- platform/core/tool/chat/Chat/AllChats.ts
+++ platform/core/tool/chat/Chat/AllChats.ts
@@ -1,10 +1,23 @@
 import { Chat, ChatId } from './Chat'
 
-export type AllChats = ReadonlyMap<ChatId, Chat | null>
+export type AllChats<Type extends Chat = Chat> = ReadonlyMap<
+  ChatId,
+  Type | null
+>
 
-export const getChat = (
-  allChats: AllChats, chatId: ChatId,
-): Chat | null => allChats.get(chatId) ?? null
+export const getChat = <Type extends Chat = Chat>(
+  allChats: AllChats<Type>,
+  chatId: ChatId,
+): Type | null => allChats.get(chatId) ?? null
 
-export const setChat = (allChats: AllChats, chat: Chat): AllChats =>
-  new Map(allChats).set(chat.id, chat)
+export const setChat = <Type extends Chat = Chat>(
+  allChats: AllChats<Type>,
+  chat: Type,
+): AllChats<Type> => new Map(allChats).set(chat.id, chat)
+
+export const getChats = <Type extends Chat = Chat>(
+  allChats: AllChats<Type>,
+): Readonly<Type[]> => {
+  const chats = Array.from(allChats.values())
+  return chats.filter((chat): chat is Type => chat !== null)
+}

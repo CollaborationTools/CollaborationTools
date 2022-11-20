ef7221138c93856e9264d87790b5a371bee75b5c
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-11-21 00:23:55 +0100

    feat: get route params or open a chat page from useRouting

--- platform/framework/composables/useRouting/useRouting.ts
+++ platform/framework/composables/useRouting/useRouting.ts
@@ -1,13 +1,16 @@
 import { ComputedRef, Ref } from 'vue'
 import { RouteLocationNormalizedLoaded } from 'vue-router'
 
-import { SPACE_PATH } from '@/config'
+import { CHAT_ID_PARAM, SPACE_ID_PARAM, spaceRoutes } from '@/config'
 import { SpaceId } from 'core/space'
+import { ChatId } from 'core/tool/chat'
 
 type UseRouting = {
   getRoute: () => Ref<RouteLocationNormalizedLoaded>
+  getRouteParameter: (parameter: string) => ComputedRef<string | null>
   goBack: () => void
   hasHistory: ComputedRef<boolean>
+  openChat: (spaceId: SpaceId, chatId: ChatId) => void
   openSpace: (spaceId: SpaceId) => void
 }
 
@@ -16,13 +19,41 @@ export default function useRouting(): UseRouting {
 
   const getRoute = (): Ref<RouteLocationNormalizedLoaded> => router.currentRoute
 
+  const getRouteParameter = (parameter: string): ComputedRef<string | null> => {
+    const route = router.currentRoute
+    return computed(() => {
+      const maybeParameter =
+        route.value.params[parameter] instanceof Array
+          ? route.value.params[parameter]?.[0]
+          : route.value.params[parameter]
+      return maybeParameter ? String(maybeParameter) : null
+    })
+  }
+
   const goBack = (): void => router.back()
 
   const hasHistory = computed(() => !!router.options.history.state.back)
 
-  const openSpace = (spaceId: SpaceId): void => {
-    router.push(`${SPACE_PATH}/${spaceId}`)
+  const open = (path: string): void => {
+    router.push(path)
   }
 
-  return { getRoute, goBack, hasHistory, openSpace }
+  const openChat = (spaceId: SpaceId, chatId: ChatId): void =>
+    open(
+      spaceRoutes.chat
+        .replace(SPACE_ID_PARAM, spaceId)
+        .replace(CHAT_ID_PARAM, chatId),
+    )
+
+  const openSpace = (spaceId: SpaceId): void =>
+    open(spaceRoutes.index.replace(SPACE_ID_PARAM, spaceId))
+
+  return {
+    getRoute,
+    getRouteParameter,
+    goBack,
+    hasHistory,
+    openChat,
+    openSpace,
+  }
 }

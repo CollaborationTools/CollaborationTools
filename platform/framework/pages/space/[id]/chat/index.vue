841fb61e4e1668822747ac55102e6c3f5c41552c
xollaborator <54781638+xollaborator@users.noreply.github.com>
2022-11-28 00:36:37 +0100

    feat: add main page for chats

---
+++ platform/framework/pages/space/[id]/chat/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <component :is="defaultChat.listComponent" />
+</template>
+
+<script setup lang="ts">
+import useSpaceStore from 'stores/useSpaceStore'
+import { defaultChat } from 'tools/_default/chat'
+
+const currentSpace = $computed(() => useSpaceStore().getCurrentSpace())
+
+useHead({
+  title: `Chats @ ${currentSpace?.name}`,
+})
+
+definePageMeta({
+  layout: 'space',
+})
+</script>
